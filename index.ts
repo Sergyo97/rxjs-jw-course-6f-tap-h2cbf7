40e4358c14f7185508b7745ade921289c7a4015e
Sergyo97 <31071190+Sergyo97@users.noreply.github.com>
2023-01-19 20:22:47 -0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+import { of } from 'rxjs';
+import { filter, map, tap } from 'rxjs/operators';
+
+of(1, 7, 3, 6)
+  .pipe(
+    filter((value) => value > 5),
+    tap((value) => console.log('Spy: ', value)),
+    map((value) => value * 2)
+  )
+  .subscribe((value) => console.log('Output:', value));
